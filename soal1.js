ca9fcefbc56b2338b84ccf3fbe55bb75d801cd50
RahmanSopian <104488328+rahmansopianqa@users.noreply.github.com>
2023-01-31 20:55:43 +0700

    add nilai1.js

---
+++ soal1.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const x = prompt('Input x: ');
+
+
+
+if(x<0){
+
+    console.log("Tidak bisa input bilangan negatif")
+
+}
+
+else if(x % 2 == 1) {
+
+    console.log("Tidak bisa input bilangan ganjil")
+
+}
+
+else (console.log(Math.sqrt(x))) 
